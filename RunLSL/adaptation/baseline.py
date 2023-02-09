fb14a660fb85e917a532440972cd1df35d326f6f
powerkarrot <powerkarrot@gmail.com>
2023-02-09 15:30:02 +0100

    some changes
    Former-commit-id: c23a11a873a2016382a43ba8912c6ee353b420db [formerly bbe90cfbc6660499b9c1621e59b8d2e06ae54a0e]
    Former-commit-id: b3b40745e9fa22adea695abdd56567ac590db4a4
    Former-commit-id: 5ab02b2c647be7d1ed8831447cd7e6094d459b18

--- RunLSL/adaptation/baseline.py
+++ RunLSL/adaptation/baseline.py
@@ -10,21 +10,16 @@ import pickle
 alpha = None
     
 #TODO: move to utils
-def calculate_iaf_power(pid, lst_eeg_values = None, baseline=False):
+def calculate_iaf_power(pid, values = None, baseline=False):
     
     if baseline:   
         raw = make_raw_csv(pid=pid, preprocess=False)
     else:
-        #print((lst_eeg_values[0:5]))
-        #print("baseline 1: ", len(lst_eeg_values))
-
-        raw = samples = np.asarray(lst_eeg_values, dtype=object)
+        raw = samples = np.asarray(values, dtype=object)
         raw = make_raw_arr(preprocess=False, samples=samples.T) 
         scan_durn = raw._data.shape[1] / raw.info['sfreq']
         print('Duration of EEG recording = ', scan_durn, 's, or', scan_durn / 60, 'min.')           
-        lst_eeg_values = []
-        #print("baseline 2: ", len(lst_eeg_values))
-
+        values = []
 
     filename = './RunLSL/IAF/pickles/' + str(pid) + '-iaf.pickle'
     with open(filename, 'rb') as handle:
@@ -34,7 +29,7 @@ def calculate_iaf_power(pid, lst_eeg_values = None, baseline=False):
     roi_power_1 = compute_freq_power(raw, alpha[0], alpha[1],  picks=roi1)  #TODO: double check which picks for iaf
     roi_power_2 = compute_freq_power(raw, theta[0], theta[1],  picks=roi2)  #TODO: double check which picks for iaf, check freq, range. ARDA CHANGE HERE
 
-    return lst_eeg_values, [roi_power_1 ,  roi_power_2]
+    return values, [roi_power_1 ,  roi_power_2]
     
 
 
